856ce9d9cc29c639622b1f4c6956a26fae2045be
anshvrma <anshverma110044@gmail.com>
2024-01-03 11:08:16 +0530

    dot game

--- py1gm.py
+++ py1gm.py
@@ -2,4 +2,37 @@ import pygame
 
 pygame.init()
 
+# screen size
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+player = pygame.Rect((300, 250, 50, 50))
+
+# loop
+run = True
+while run:
+
+    screen.fill((0, 0, 0))
+
+    pygame.draw.rect(screen, (255, 0, 0), player)
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_a] == True:
+        player.move_ip(-1, 0)
+    if key[pygame.K_d] == True:
+        player.move_ip(1, 0)
+    if key[pygame.K_w] == True:
+        player.move_ip(0, -1)
+    if key[pygame.K_s] == True:
+        player.move_ip(0, 1)
+
+# event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+
+pygame.quit()
