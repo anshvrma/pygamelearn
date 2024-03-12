a98dce13c7a16400cdeae14b209b6a7cb7ee23d9
anshvrma <anshverma110044@gmail.com>
2024-03-12 17:26:43 +0530

    part 1

---
+++ tic-tac-toe.py
@@ -0,0 +1,54 @@
+import pygame
+from pygame import *
+
+pygame.init()
+
+screen_width = 300
+screen_height = 300
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Tic-Tac-Toe')
+
+# define variable
+line_width = 5
+markers = []
+clicked = False
+pos = []
+player = 1
+
+def draw_grid():
+    bg = (255, 255, 200)
+    grid = (50, 50, 50)
+    screen.fill(bg)
+    for x in range(1, 3):
+        pygame.draw.line(screen, grid, (0, x * 100), (screen_width, x * 100), line_width)
+        pygame.draw.line(screen, grid, (x * 100, 0), (x * 100, screen_width), line_width)
+
+for x in range(3):
+    row = [0] * 3
+    markers.append(row)
+
+run = True
+while run:
+
+    draw_grid()
+
+    # event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+            clicked = True
+        if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+            clicked = False
+            pos = pygame.mouse.get_pos()
+            cell_x = pos[0]
+            cell_y = pos[1]
+            if markers[cell_x // 100][cell_y // 100] == 0:
+                markers[cell_x // 100][cell_y // 100] = player
+                player *= -1
+
+    pygame.display.update()
+
+pygame.quit()
+
